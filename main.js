ec24e37d6867aafc7a154453bbf6f99fe3732aad
MDubach <michadubach@gmail.com>
2023-10-25 16:04:47 +0200

    play navigation

--- main.js
+++ main.js
@@ -25,48 +25,58 @@ const gridHelper = new THREE.GridHelper(200,50);
 scene.add(ambientLight, gridHelper);
 
 // liltecca video einbindung
-const lilteccaGeometry = new THREE.SphereGeometry(5,32,32);
+const lilteccaGeometry = new THREE.SphereGeometry(13,32,32);
 const lilteccaVideo = document.getElementById("lilteccavideo");
 const lilteccaTexture = new THREE.VideoTexture(lilteccaVideo);
 const lilteccaMaterial = new THREE.MeshBasicMaterial({ map: lilteccaTexture });
 const lilteccaSphere = new THREE.Mesh(lilteccaGeometry, lilteccaMaterial);
-lilteccaSphere.position.set(30, 0, 0);
-lilteccaSphere.name = 'lilteccaSphere';
+lilteccaSphere.name = "lilteccaSphere";
+lilteccaSphere.videoname = "Lil Tecca - 500lbs";
+console.log(lilteccaSphere);
 
 // add liltecca video to scene
 scene.background = lilteccaTexture;
 scene.add(lilteccaSphere);
 
 // yeat video einbindung
-const yeatGeometry = new THREE.TorusGeometry(10,3,16,100);
+const yeatGeometry = new THREE.DodecahedronGeometry(13,0);
 const yeatVideo = document.getElementById("yeatvideo");
 const yeatTexture = new THREE.VideoTexture(yeatVideo);
 const yeatMaterial = new THREE.MeshBasicMaterial({ map: yeatTexture });
 const yeatTorus = new THREE.Mesh(yeatGeometry, yeatMaterial);
-yeatTorus.name = 'yeatTorus';
+yeatTorus.position.set(-30, 0, 0);
+yeatTorus.name = "yeatTorus";
+yeatTorus.videoname = "Yeat - bigger then everything";
 
 // add yeat video to scene
 scene.add(yeatTorus);
 
 
 // travis video einbindung
-const travisGeometry = new THREE.IcosahedronGeometry(16,0);
+const travisGeometry = new THREE.IcosahedronGeometry(13,0);
 const travisVideo = document.getElementById("travisvideo");
 const travisTexture = new THREE.VideoTexture(travisVideo);
 const travisMaterial = new THREE.MeshBasicMaterial({ map: travisTexture });
 const travisIcosahedron = new THREE.Mesh(travisGeometry, travisMaterial);
-travisIcosahedron.position.set(60,0,0);
-travisIcosahedron.name = 'travisIcosahedron';
+travisIcosahedron.position.set(30,0,0);
+travisIcosahedron.name = "travisIcosahedron";
+travisIcosahedron.videoname = "Travis Scott - Escape Plan";
 
 // add travis video to scene
 scene.add(travisIcosahedron);
+
+// selected mesh and song
+let selectedMesh = lilteccaSphere;
+
+// song is playing boolean
+let isPlaying = false;
 
 // Orbitcontrols for movement
 const controls = new OrbitControls(camera, renderer.domElement);
 
 // add stars to scene
 function addStar() {
-    const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+    const geometry = new THREE.SphereGeometry(0.2, 24, 24);
     const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
     const star = new THREE.Mesh( geometry, material );
 
@@ -84,11 +94,7 @@ document.body.onkeyup = function(e) {
       e.code == "Space" ||      
       e.keyCode == 32      
   ) {
-    if (lilteccaVideo.paused) {
-      lilteccaVideo.play();
-    } else {
-      lilteccaVideo.pause();
-    }
+    playOrStopVideo();
   }
 }
 
@@ -121,32 +127,44 @@ window.addEventListener('click', onMouseClick);
 
 
 function onMeshClick(mesh) {
+
+  if (mesh.name == 'lilteccaSphere') {
+    document.getElementById('infoText').innerHTML = lilteccaSphere.videoname;
+    selectedMesh = lilteccaSphere;
+    scene.background = lilteccaTexture;
+  } else if (mesh.name == 'yeatTorus') {
+    document.getElementById('infoText').innerHTML = yeatTorus.videoname;
+    selectedMesh = yeatTorus;
+    scene.background = yeatTexture;
+  } else if (mesh.name == 'travisIcosahedron') {
+    document.getElementById('infoText').innerHTML = travisIcosahedron.videoname;
+    selectedMesh = travisIcosahedron;
+    scene.background = travisTexture;
+  }
+
+  lilteccaVideo.pause();
+  yeatVideo.pause();
+  travisVideo.pause();
+
+  isPlaying = false;
+  playButton.innerHTML = "play";
+
   // Perform your actions when a mesh is clicked
   console.log(`Mesh clicked: ${mesh.name}`);
 }
 
-
-
-/*function onMeshClick(event, intersectsMesh) {
-  console.log(intersectsMesh);
-  // Perform your actions when the mesh is clicked
-  if (lilteccaVideo.paused) {
-    scene.background = lilteccaTexture;
-    lilteccaVideo.play();
-    travisVideo.pause();
-  } else {
-    lilteccaVideo.pause();
-    scene.background = travisTexture;
-    travisVideo.play();
-  }
-  console.log('Mesh clicked!');
-}*/
-
 function animate() {
   requestAnimationFrame(animate);
+
+  /*lilteccaSphere.rotation.x += 0.001;
+  lilteccaSphere.rotation.y += 0.008;
+  lilteccaSphere.rotation.z += 0.002;
+
   yeatTorus.rotation.x += 0.01;
   yeatTorus.rotation.y += 0.005;
-  yeatTorus.rotation.z += 0.01;
+  yeatTorus.rotation.z += 0.01;*/
+
+
 
   controls.update();
 
@@ -155,3 +173,52 @@ function animate() {
 }
 
 animate();
+
+
+// playsettings controllers
+const resetButton = document.getElementById("resetButton");
+const playButton = document.getElementById("playButton");
+const nextVideoButton = document.getElementById("nextVideoButton");
+
+resetButton.addEventListener("click", function() {
+  lilteccaVideo.load();
+  yeatVideo.load();
+  travisVideo.load();
+
+});
+
+playButton.addEventListener("click", function() {
+  playOrStopVideo();
+});
+
+nextVideoButton.addEventListener("click", function() {
+
+  console.log("hdlkafjsdlfkjasdlkf");
+
+});
+
+function playOrStopVideo() {
+
+  if (isPlaying) {
+    lilteccaVideo.pause();
+    yeatVideo.pause();
+    travisVideo.pause();
+    isPlaying = false;
+    playButton.innerHTML = "play";
+  } else {
+    if (selectedMesh.name == 'lilteccaSphere') {
+      document.getElementById('infoText').innerHTML = lilteccaSphere.videoname;
+      lilteccaVideo.play();
+    } else if (selectedMesh.name == 'yeatTorus') {
+      document.getElementById('infoText').innerHTML = yeatTorus.videoname;
+      yeatVideo.play();
+    } else if (selectedMesh.name == 'travisIcosahedron') {
+      document.getElementById('infoText').innerHTML = travisIcosahedron.videoname;
+      travisVideo.play();
+    }
+  
+    isPlaying = true;
+    playButton.innerHTML = "stop";
+  }
+
+}
